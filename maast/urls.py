5eea21ac5a5775396495cff7a4ba1a0b867da3a4
Tim Wilson <tim@timwilson.info>
2023-12-14 22:17:53 -0600

    Add modal button to navbar

--- maast/urls.py
+++ maast/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from maast.views.events import get_valid_scores_by_event
+from maast.views.home import home_page_view
 from maast.views.persons import (
     get_valid_data_by_person,
     get_valid_person_scores,
@@ -11,7 +12,7 @@ from maast.views.records import get_valid_records_by_round
 from maast.views.scores import get_valid_scores_by_round_and_division
 
 urlpatterns = [
-    # path("", home_page_view, name="home"),
+    path("", home_page_view, name="home"),
     path("profile/<str:person_slug>", get_valid_data_by_person, name="person_profile"),
     path("records/<int:round_id>", get_valid_records_by_round, name="round_records"),
     path(
